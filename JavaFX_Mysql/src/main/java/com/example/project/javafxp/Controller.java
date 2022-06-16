431313b7a2b327bb013a3d5243121a6f681ae3cb
ClaudiaYLopezLafita <alum.clopezl.2021@iesalixar.org>
2022-06-16 22:08:55 +0200

    Conexión a bd y visualizacion de la tabla empleados.

--- JavaFX_Mysql/src/main/java/com/example/project/javafxp/Controller.java
+++ JavaFX_Mysql/src/main/java/com/example/project/javafxp/Controller.java
@@ -1,10 +1,5 @@
 package com.example.project.javafxp;
 
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -14,12 +9,8 @@ import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Optional;
 
 public class Controller {
@@ -28,48 +19,6 @@ public class Controller {
     private TextField txtName; //Campo "Nombre" del formulario
     @FXML
     private PasswordField txtPassword; //Campo "Password" del formulario
-
-    private TableView<Employees> tvEmployees;
-    @FXML
-    private TableView tableview;
-
-    private ObservableList<Employees> data;
-
-    @FXML
-    private TableColumn<Employees, Integer> employeeNumberCol;
-    @FXML
-    private TableColumn<Employees, String> lastNameCol;
-    @FXML
-    private TableColumn<Employees, String> firstNameCol;
-    @FXML
-    private TableColumn<Employees, String> extensionCol;
-    @FXML
-    private TableColumn<Employees, String> emailCol;
-    @FXML
-    private TableColumn<Employees, String> officeCodeCol;
-    @FXML
-    private TableColumn<Employees, Integer> reportsToCol;
-    @FXML
-    private TableColumn<Employees, String> titleCol;
-
-    /**
-     * Initializes the controller class. This method is automatically called
-     * after the fxml file has been loaded.
-     * Supuestamente deberia inicializar las columnas. Comentado porque casca con NullPointerException
-     */
-
-    /*@FXML
-    private void initialize() {
-        // Initialize the person table with the two columns.
-        employeeNumberCol.setCellValueFactory(cellData -> cellData.getValue().employeeNumberProperty());
-        firstNameCol.setCellValueFactory(cellData -> cellData.getValue().firstNameProperty());
-        lastNameCol.setCellValueFactory(cellData -> cellData.getValue().lastNameProperty());
-        extensionCol.setCellValueFactory(cellData -> cellData.getValue().extensionProperty());
-        emailCol.setCellValueFactory(cellData -> cellData.getValue().emailProperty());
-        officeCodeCol.setCellValueFactory(cellData -> cellData.getValue().officeCodeProperty());
-        reportsToCol.setCellValueFactory(cellData -> cellData.getValue().reportToProperty());
-        titleCol.setCellValueFactory(cellData -> cellData.getValue().titleProperty());
-    }*/
 
     public void btLimpiarClick(ActionEvent actionEvent) {
         txtPassword.setText("");
@@ -90,7 +39,7 @@ public class Controller {
             alert.setHeaderText("Faltan datos por introducir");
             alert.setContentText("Compruebe que el nombre y la contraseña estan rellenos. ");
             txtName.requestFocus();
-        } else if ( !nombre.equals("Claudia") || !password.equals("123456")){
+        } else if ( !nombre.equals("Claudia") || !password.equals("123")){
             alert = new Alert(Alert.AlertType.WARNING);
             alert.setTitle("Datos incorrectos...");
             alert.setHeaderText("El nombre y/o la contraseña son incorrectas.");
@@ -112,7 +61,7 @@ public class Controller {
                 throw new RuntimeException(e);
             }
         }
-        alert.showAndWait();
+//        alert.showAndWait();
     }
 
     public void btSalirClick(ActionEvent actionEvent) {
@@ -135,72 +84,15 @@ public class Controller {
 
         try {
             root = FXMLLoader.load(
-                    Controller.class.getResource("employees.fxml"));
-
+                    Main.class.getResource("employees.fxml"));
             stage.setScene(new Scene(root));
-           stage.setTitle("Employees");
+           stage.setTitle("Employe");
             stage.initModality(Modality.WINDOW_MODAL);
             stage.initOwner(
                     ((Node) actionEvent.getSource()).getScene().getWindow() );
-            builDataEmployees2();
-
             stage.show();
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    public void builDataEmployees1(){
-        Connection connection = ConnectionDB.getConnection();
-        data = FXCollections.observableArrayList();
-        String SQL = "SELECT * FROM employees;";
-        try {
-            ResultSet rs = connection.createStatement().executeQuery(SQL);
-
-            for(int i=0 ; i<rs.getMetaData().getColumnCount(); i++){
-                //We are using non property style for making dynamic table
-                final int j = i;
-                TableColumn col = new TableColumn(rs.getMetaData().getColumnName(i+1));
-
-                col.setCellValueFactory(
-                        (Callback<TableColumn.CellDataFeatures<ObservableList, String>, ObservableValue<String>>)
-                                param -> new SimpleStringProperty(param.getValue().get(j).toString()));
-
-                tableview.getColumns().addAll(col);
-                System.out.println("Column ["+i+"] ");
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error on Building Data");
-        }
-    }
-
-    public void builDataEmployees2(){
-        Connection connection = ConnectionDB.getConnection();
-        data = FXCollections.observableArrayList();
-        String SQL = "SELECT * FROM employees;";
-        try {
-            ResultSet rs = connection.createStatement().executeQuery(SQL);
-
-            while (rs.next()) {
-                //Iterate Row
-                ObservableList<String> row = FXCollections.observableArrayList();
-                for (int k = 1; k <= rs.getMetaData().getColumnCount(); k++) {
-                    //Iterate Column
-                    row.add(rs.getString(k));
-                }
-                System.out.println("Row [1] added " + row);
-                Employees emp = new Employees();
-                emp = emp.getEmployee(row);
-                //data.add(row);
-                data.add(emp);
-            }
-            //FINALLY ADDED TO TableView. CASCA porque dice que esta a null
-            tvEmployees.setItems(data);
-
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Error on Building Data");
         }
     }
 
@@ -220,6 +112,5 @@ public class Controller {
             throw new RuntimeException(e);
         }
     }
-
 
 }
--- JavaFX_Mysql/src/main/java/module-info.java
+++ JavaFX_Mysql/src/main/java/module-info.java
@@ -6,4 +6,9 @@ module com.example.project.javafxp {
 
     opens com.example.project.javafxp to javafx.fxml;
     exports com.example.project.javafxp;
+//    exports com.example.project.javafxp.Controller;
+//    opens com.example.project.javafxp.Controller to javafx.fxml;
+//    exports com.example.project.javafxp.Model;
+//    opens com.example.project.javafxp.Model to javafx.fxml;
+//    exports com.example.project.javafxp;
 }
